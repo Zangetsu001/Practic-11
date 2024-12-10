afed9af8d233accf7db6e6124dd933b4f1abf111
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2024-12-10 11:22:52 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        /// <summary>
+        /// Найти максимальное\минимальное значение массива
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int[] omas = { 10, 7, 9, -8, 13, -5 };
+            int max = omas.Max();
+            int min = omas.Min();
+            Console.WriteLine($"Максимальное: {max}\nМинимальное: {min}");
+            Console.ReadKey();
+        }
+    }
+}
