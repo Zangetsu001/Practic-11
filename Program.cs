c36e7c7a3631d828f1b7e34db0158e8a513399c1
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2024-12-10 11:23:26 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] omas = { "Яблоко", "Груша", "Ананас" };
+            Array.Reverse(omas);
+            foreach(var el in omas)
+            {
+                Console.WriteLine(el);
+            }
+            Array.Sort(omas);
+            foreach(var el in omas)
+            {
+                Console.WriteLine(el);
+            }
+            Console.ReadKey();
+        }
+    }
+}
